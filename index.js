40f3bb85e3213ac678b2fe365f141c5262711f15
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-25 22:41:38 +0200

    Add task 2 and 3

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 "use strict";
-const btn = document.querySelector('#btn');
 
-function greetingUser(event){
- alert('Hello, clicker!')
-}
-btn.addEventListener('click', greetingUser)
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", greetingUser);
+function greetingUser(event) {
+  alert("Hello, clicker!");
+};
+
+const link = document.querySelector("#link");
+const hiddenBtn = document.querySelector("#hiddenBtn");
+hiddenBtn.style.display = "none";
+
+link.addEventListener("click", newButtonAppears);
+function newButtonAppears(event) {
+  event.preventDefault();
+  hiddenBtn.style.display = "inline-block";
+};
+
+const lamp = document.querySelector("#lamp");
+const lampBtn = document.querySelector("#lampBtn");
+
+lampBtn.addEventListener("click", lampOn);
+function lampOn(event) {
+  event.preventDefault();
+  lamp.classList.toggle("lamp-on");
+};
