ac4ffaba96d707120b2f97a8fcf5b24b6537fae9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-26 16:55:56 +0200

    Add version2 to task 2

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ btn.addEventListener("click", greetingUser);
 function greetingUser(event) {
   alert("Hello, clicker!");
 };
-
+//----------------------------------------------
+//version1
 const link = document.querySelector("#link");
 const hiddenBtn = document.querySelector("#hiddenBtn");
 hiddenBtn.style.display = "none";
@@ -17,6 +18,22 @@ function newButtonAppears(event) {
   hiddenBtn.style.display = "inline-block";
 };
 
+//version2
+/*const link = document.querySelector("#link");
+
+const hiddenBtn = document.createElement('button');
+hiddenBtn.append('Hidden button');
+document.body.append(hiddenBtn);
+link.parentNode.append(hiddenBtn);
+hiddenBtn.style.display = "none";
+
+link.addEventListener("click", newButtonAppears);
+function newButtonAppears(event) {
+  event.preventDefault();
+  hiddenBtn.style.display = "inline-block";
+};*/
+
+//----------------------------------------------------
 const lamp = document.querySelector("#lamp");
 const lampBtn = document.querySelector("#lampBtn");
 
